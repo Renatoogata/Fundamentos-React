f6a40614bd29831484e1532888a4ab1642a8fb15
Renatoogata <renatoogata95@gmail.com>
2022-08-17 18:17:37 -0300

    Comment, avatar

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src='https://github.com/Renatoogata.png' alt='' />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Diego Fernandes</strong>
+                            <time title="11 de Maio as 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atras</time>
+                        </div>
+
+                        <button title="Deletar um comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabens!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    )
+}
